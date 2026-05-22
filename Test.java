22fcd33ebb72fe5bff7a3667e8a0eec9908aa610
iter-palak-goyal <iter.palak.goyal@gmail.com>
2026-05-22 11:37:08 +0530

    updated Test.java

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test {
     int b=57;
     int result = a+b;
     //result is multiplication of two numbers
-    System.out.println("The product of a and b is " +result);
+    System.out.println("The sum of a and b is " +result);
     
   }
   public static void main(String[] args)
